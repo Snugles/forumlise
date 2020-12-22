ef7c9892ea9ed809981e610d0823537339d1963e
snuglyWugly <peter.r.ackroyd@gmail.com>
2020-12-22 19:54:52 +0000

    feat:added express server

---
+++ server/index.ts
@@ -0,0 +1,15 @@
+'use strict';
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const router = require('./router');
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+// app.use(router);
+
+app.listen( port, () => {
+  console.log( `server started at http://localhost:${ port }` );
+} );
