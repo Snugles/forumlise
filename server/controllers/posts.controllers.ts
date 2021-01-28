f16a37840c3385ca2a8211987c7b82e5e733e5d4
snuglyWugly <peter.r.ackroyd@gmail.com>
2021-01-28 11:50:49 +0000

    feat:added api to manage creation of posts

--- server/controllers/posts.controllers.ts
+++ server/controllers/posts.controllers.ts
@@ -2,13 +2,13 @@
 
 const PostModel = require('../models/Post.models.ts');
 
-exports.getAll = async (req:any, res:any) => {
+exports.getAll = async (_:any, res:any) => {
   try {
     PostModel.findAll()
       .then((data:Array<any>)=>{
         console.log(data);
-        res.send(data);
         res.status(200);
+        res.send(data);
       });
   } catch (e) {
     console.error('e', e);
@@ -18,9 +18,12 @@ exports.getAll = async (req:any, res:any) => {
 
 exports.postOne = async (req:any, res:any) => {
   try {
-    console.log('posted');
-    res.status(201);
-    res.send('data');
+    PostModel.create(req.body)
+      .then((data:Array<any>)=>{
+        console.log(data);
+        res.status(201);
+        res.send(data);
+      });
   } catch (e) {
     console.error('e', e);
     res.sendStatus(500);
